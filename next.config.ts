b32418030e9611db135d1f3ce64687d554c8e28a
GoldPass <60227424+GoldPass@users.noreply.github.com>
2025-08-02 02:19:15 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,11 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+const path = require("path");
+
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
+};
