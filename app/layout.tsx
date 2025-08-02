249ca13d833ffcb65b95805d84318e4b04549083
GoldPass <60227424+GoldPass@users.noreply.github.com>
2025-08-02 13:46:03 +0300

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import Footer from "@/app/components/Footer";
+
+export const metadata = {
+  title: "Портфолио разработчика",
+  description: "Сайт-портфолио Алексея",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ru">
+      <body className="min-h-screen bg-gradient-to-br from-gray-50 to-blue-50">
+        <main className="pt-4 pb-32 pl-4">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
