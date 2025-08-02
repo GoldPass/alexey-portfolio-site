60e47a058f5341defc21158e857fe6e07f4fd0c9
GoldPass <60227424+GoldPass@users.noreply.github.com>
2025-08-02 13:47:03 +0300

    Add files via upload

---
+++ types/global.d.ts
@@ -0,0 +1,11 @@
+declare module "../../components/Header" {
+  import { ReactElement } from "react";
+  const Header: () => ReactElement;
+  export default Header;
+}
+
+declare module "../../components/Footer" {
+  import { ReactElement } from "react";
+  const Footer: () => ReactElement;
+  export default Footer;
+}
